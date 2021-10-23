a82bc67cd7b7d59fed81b5f79569cf2d67bb5e8e
Emrick19 <emricky17@gmail.com>
2021-10-23 21:18:57 +0100

    Displayed api's data

--- components/modules/Info.tsx
+++ components/modules/Info.tsx
@@ -1,42 +1,51 @@
-import { Paper, Grid, Typography, Divider } from "@mui/material";
+import { Paper, Grid, Typography } from "@mui/material";
 import React from "react";
 
-const Info = () => {
+const Info = ({ data }: any) => {
   return (
     <Paper
       elevation={4}
       sx={{
-        maxWidth: "80%",
+        maxWidth: "87%",
         mx: "auto",
         padding: 4,
-        textAlign: "center",
         borderRadius: 2,
+        position: "relative",
+        top: -80,
       }}
     >
-      <Grid container spacing={2}>
-        <Grid item xs={12} lg={3}>
+      <Grid container spacing={1}>
+        <Grid item xs={12} lg={4} sx={{ borderRight: "1px solid #d3d3d3" }}>
           <Typography variant="subtitle1" color="gray">
             Ip Address
           </Typography>
-          <Typography variant="h4">192.212.174.101</Typography>
+          <Typography variant="h4" sx={{ fontWeight: "bold" }}>
+            {data.ip}
+          </Typography>
         </Grid>
-        <Grid item xs={12} lg={3}>
+        <Grid item xs={12} lg={2} sx={{ borderRight: "1px solid #d3d3d3" }}>
           <Typography variant="subtitle1" color="gray">
             Location
           </Typography>
-          <Typography variant="h4">Brooklyn, NY 10001</Typography>
+          <Typography variant="h4" sx={{ fontWeight: "bold" }}>
+            {data.location.region}
+          </Typography>
         </Grid>
-        <Grid item xs={12} lg={3}>
+        <Grid item xs={12} lg={3} sx={{ borderRight: "1px solid #d3d3d3" }}>
           <Typography variant="subtitle1" color="gray">
             Timezone
           </Typography>
-          <Typography variant="h4">UTC -05:00</Typography>
+          <Typography variant="h4" sx={{ fontWeight: "bold" }}>
+            UTC {data.location.timezone}
+          </Typography>
         </Grid>
-        <Grid item xs={12} lg={3}>
+        <Grid item xs={12} lg={2}>
           <Typography variant="subtitle1" color="gray">
             ISP
           </Typography>
-          <Typography variant="h4">SpaceX Starlink</Typography>
+          <Typography variant="h4" sx={{ fontWeight: "bold" }}>
+            {data.isp}
+          </Typography>
         </Grid>
       </Grid>
     </Paper>
