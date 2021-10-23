7fc0c3229b4a70998c5249f0ccbc3bda508148dc
Emrick19 <emricky17@gmail.com>
2021-10-23 21:22:53 +0100

    Fetch data from apis

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,29 @@
-import type { NextPage } from "next";
+import type { NextPage, GetStaticProps } from "next";
+
 import Layout from "../components/ui/Layout";
 import Header from "../components/modules/Header";
 import Info from "../components/modules/Info";
 
-const Home: NextPage = () => {
+const Home: NextPage = ({ data }: any) => {
   return (
     <Layout>
       <Header />
       <main>
-        <Info />
+        <Info data={data} />
       </main>
     </Layout>
   );
+};
+export const getStaticProps: GetStaticProps = async () => {
+  const res = await fetch(
+    "https://geo.ipify.org/api/v2/country?apiKey=at_bAZzx5GQCnFyTVK25Qx5XXRyA2wxw"
+  );
+  const data = await res.json();
+  return {
+    props: {
+      data,
+    },
+  };
 };
 
 export default Home;
